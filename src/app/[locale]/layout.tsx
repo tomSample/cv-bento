fb373bb614d0faaea33591dff49af5129bf7c802
Thomas Grandjean <thomasgrandjean45@gmail.com>
2025-11-03 10:57:47 +0100

    update pour permettre déploiement vercel

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -89,7 +89,7 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
   
